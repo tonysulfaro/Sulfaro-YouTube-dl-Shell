ab0c08161564cece7d8d8c47776967223eafd701
Tony <Tony@ANTHONY>
2017-02-24 16:12:59 -0500

    alpha build

--- Sulfaro YouTube-dl Shell/Form1.vb
+++ Sulfaro YouTube-dl Shell/Form1.vb
@@ -2,7 +2,7 @@
 
 Public Class frmYouTubeDL
 
-
+    'Opens file browser to specify target directory in txtFilePath.Text
     Public Sub brnFileBrowse_Click(sender As Object, e As EventArgs) Handles brnFileBrowse.Click
         If (FolderBrowserDialog1.ShowDialog() = DialogResult.OK) Then
             txtFilePath.Text = FolderBrowserDialog1.SelectedPath
@@ -11,17 +11,26 @@ Public Class frmYouTubeDL
 
     Public Sub btnDownload_Click(sender As Object, e As EventArgs) Handles btnDownload.Click
 
-        Dim youtubedlDir As String = "z:\other\youtube\youtube-dl.exe"
-        Dim filepath As String = txtFilePath.Text
 
-        If Not Directory.Exists(filepath) Then
-            Directory.CreateDirectory(filepath)
-        End If
-        Dim file = New FileInfo(youtubedlDir)
-        file.CopyTo(Path.Combine(filepath, file.Name), True)
+        'depreciated code that ended up being too complicated.
+        'Dim youtubedlDir As String = "z:\other\youtube\youtube-dl.exe"
+        'Dim filepath As String = txtFilePath.Text
 
+        'If Not Directory.Exists(filepath) Then
+        '    Directory.CreateDirectory(filepath)
+        'End If
+        'Dim file = New FileInfo(youtubedlDir)
+        'file.CopyTo(Path.Combine(filepath, file.Name), True)
+
+        'change dir to specified file path
         ChDir(txtFilePath.Text)
+
+        'shell to update youtube-dl
+        'Shell("cmd.exe /k " + "youtube-dl -U")
+
+        'shell to run cmd.exe and youtube-dl with the url from txtSource.Text
         Shell("cmd.exe /k " + "youtube-dl " + txtSource.Text)
+
 
     End Sub
 
